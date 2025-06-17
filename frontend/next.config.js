512e635650b4202439ade5222a5382e77739be08
jaidivyakl1002 <jaidivyakumarlohani@gmail.com>
2025-06-17 10:58:29 +0530

    next.config.js revert

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -4,11 +4,6 @@ const withNextPluginPreval = createNextPluginPreval();
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
-  output: 'export',
-  trailingSlash: true,
-  images: {
-    unoptimized: true
-  }
 };
 
 module.exports = withNextPluginPreval(nextConfig);
