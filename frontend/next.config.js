d45bfdecce6781a0839ec8face5fdf5400e14f6c
jaidivyakl1002 <jaidivyakumarlohani@gmail.com>
2025-06-17 10:47:20 +0530

    next.config.js

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -4,6 +4,11 @@ const withNextPluginPreval = createNextPluginPreval();
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
+  output: 'export',
+  trailingSlash: true,
+  images: {
+    unoptimized: true
+  }
 };
 
 module.exports = withNextPluginPreval(nextConfig);
